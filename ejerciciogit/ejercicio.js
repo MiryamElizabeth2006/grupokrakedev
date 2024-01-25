199eb132dd26e6342ecc3ec2e550636ce19d4750
MiryamElizabeth2006 <miryamelizabeth367@gmail.com>
2024-01-24 19:38:15 -0500

    boton agrega imagen

--- ejerciciogit/ejercicio.js
+++ ejerciciogit/ejercicio.js
@@ -1,3 +1,6 @@
 saludar = function(){
+    let cmpimagenSaludo;
+    cmpimagenSaludo = document.getElementById("imgsaludo");
+    cmpimagenSaludo.src = "./imagenes/ganar.gif"
     console.log("ingresar a saludar")
 }
