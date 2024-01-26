08f132ecf2e1beecb36e9d1e2e08a5809300d4f7
MiryamElizabeth2006 <miryamelizabeth367@gmail.com>
2024-01-26 10:21:18 -0500

    Boton limpiar agregado todos los cambios hechos

--- puntoDeVenta_v4/puntoDeVenta.js
+++ puntoDeVenta_v4/puntoDeVenta.js
@@ -73,14 +73,24 @@
             Ejemplo: 
                 Valor a pagar por 20 cerveza corona con 10% de descuento: USD 48.75
             Si funciona, hacer un commit
-        */ 
-    
+        */     
 }   
+
 limpiar=function(){
     /*
         Dejar todas las cajas de texto con el valor cadena vacía, 0 ó 0.0 según el tipo de dato
         Dejar todos los textos de los montos con el valor 0.0
         Si funciona, hacer un commit
      */
-}
+    mostrarTextoEnCaja("txtProducto", "");
+    mostrarTextoEnCaja("txtCantidad", "");
+    mostrarTextoEnCaja("txtPrecio", "");
+    mostrarTextoEnCaja("txtPorcentajeDescuento", "");
+
+    mostrarTexto("lblSubtotal", "0.0");
+    mostrarTexto("lblDescuento", "0.0");
+    mostrarTexto("lblValorIVA", "0.0");
+    mostrarTexto("lblTotal", "0.0");
+    mostrarTexto("lblResumen", "0.0");
+    }
 /* SI TODO FUNCIONA, HACER UN PUSH */
