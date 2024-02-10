edf2bd40a8641b78ec97465dda37a1aafbe5d56f
MiryamElizabeth2006 <miryamelizabeth367@gmail.com>
2024-02-10 12:57:00 -0500

    Paso 1 realizado del juego ahorcado funcion guardarPalabra

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -1,4 +1,8 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+//Crear Función
+
+let palabraSecreta;
+
 esMayuscula = function(caracter){
      if (caracter >= 'A' && caracter <= 'Z') {
         return true;
@@ -6,3 +10,23 @@ esMayuscula = function(caracter){
         return false;
     }
 }
+
+//Ingresar Palabra Secreta
+guardarPalabra = function(){
+    let recuperar = recuperarTexto("txtSecreta");
+    let longitudCaracter = recuperar.length;
+    mayuscula = esMayuscula(recuperar);
+
+    if(longitudCaracter == 5){
+        if(mayuscula == true){
+            palabraSecreta = mayuscula;
+            console.log(recuperar);
+        }else if(mayuscula == false){
+            alert("Debe ingresar una palabra de 5 letras mayusculas");
+        }
+    }else{
+        alert("Debe ingresar una palabra de 5 letras mayusculas");
+    }    
+}
+
+
