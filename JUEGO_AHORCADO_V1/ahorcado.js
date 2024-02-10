37efc23f7ec64f7b90da1eba511245d2ac7c2c55
MiryamElizabeth2006 <miryamelizabeth367@gmail.com>
2024-02-10 12:18:03 -0500

    Paso 1 realizado 1er commit del juego ahorcado

---
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -0,0 +1,8 @@
+//No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+esMayuscula = function(caracter){
+     if (caracter >= 'A' && caracter <= 'Z') {
+        return true;
+    }else{
+        return false;
+    }
+}
