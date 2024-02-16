897dde39f1c77ef955c5c06113950e78af3f2b17
MiryamElizabeth2006 <miryamelizabeth367@gmail.com>
2024-02-15 22:15:39 -0500

    Reto 46 realizado Aplicar tabla desde js y estilo a la tabla

--- roles/roles.js
+++ roles/roles.js
@@ -1,12 +1,15 @@
 let empleados = [
     {cedula:"1714616123",nombre:"John",apellido:"Cena",sueldo:500.0},
-    {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0}
+    {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0},
+    {cedula:"1745646732",nombre:"Mariana",apellido:"Sanchez",sueldo:600.0}
 ]
 
 mostrarOpcionEmpleado = function(){
     mostrarComponente("divEmpleado");
     ocultarComponente("divRol");
     ocultarComponente("divResumen");
+
+    mostrarEmpleados();
 }
 
 mostrarOpcionRol = function(){
@@ -19,4 +22,28 @@ mostrarOpcionResumen = function(){
     mostrarComponente("divResumen");
     ocultarComponente("divEmpleado");
     ocultarComponente("divRol");
+}
+
+mostrarEmpleados = function(){
+    let cmpTabla = document.getElementById("tablaEmpleados");
+    let contenidoTabla = "<table> <tr>"+
+    "<th>CEDULA</th>"+
+    "<th>NOMBRE</th>"+
+    "<th>APELLIDO</th>"+
+    "<th>SUELDO</th>"+
+
+    "</tr>";
+    let elementoEmpleado;
+    for(let i=0;i<empleados.length;i++){
+        elementoEmpleado = empleados[i];
+
+        contenidoTabla += "<tr><td>"+elementoEmpleado.cedula+"</td>"
+        +"<td>"+elementoEmpleado.nombre+"</td>"
+        +"<td>"+elementoEmpleado.apellido+"</td>"
+        +"<td>"+elementoEmpleado.sueldo+"</td>"
+        +"</tr>";
+
+    }
+    contenidoTabla += "</table>"
+    cmpTabla.innerHTML=contenidoTabla; 
 }
